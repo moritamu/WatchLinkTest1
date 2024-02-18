d062bf31e97e848276e3ea1fa4a49f36d5460926
morisum <moris@r.sannet.ne.jp>
2024-02-18 10:01:34 +0900

    作成済

--- WatchLinkTest1 Watch App/ContentView.swift
+++ WatchLinkTest1 Watch App/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        NavigationStack {
+            NavigationLink {
+                SecondView()
+            } label: {
+                Text("SecondViewへ")
+            }
+       }
         .padding()
     }
 }
---
+++ WatchLinkTest1 Watch App/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  WatchLinkTest1 Watch App
+//
+//  Created by MsMacM on 2024/02/18.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
