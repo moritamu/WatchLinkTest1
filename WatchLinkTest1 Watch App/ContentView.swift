e99a7b0c8c1800a73ce3236bd0435e09b72ee107
morisum <moris@r.sannet.ne.jp>
2024-02-18 09:50:54 +0900

    Initial Commit

---
+++ WatchLinkTest1 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLinkTest1 Watch App
+//
+//  Created by MsMacM on 2024/02/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchLinkTest1 Watch App/WatchLinkTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLinkTest1App.swift
+//  WatchLinkTest1 Watch App
+//
+//  Created by MsMacM on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct WatchLinkTest1_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
